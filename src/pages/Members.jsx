866947bfe68543356b27d990da5226ecee9af619
柳澤優貴愛 <yanagisawayuukiai@YukiAir.local>
2025-05-03 20:44:42 +0900

    Membersページの大枠

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,1 +1,12 @@
-export default function Members() {}
+export default function Members() {
+    return (
+        <main className="w-full min-h-screen bg-white">
+          <section className="py-16 px-6 bg-gradient-to-b from-green-100 to-white">
+            <h2 className="text-3xl md:text-5xl font-bold mb-4">Members</h2>
+            <p className="text-base md:text-lg text-gray-700 max-w-2xl">
+              ここにMembersページの内容が入るので記入お願いします
+            </p>
+          </section>
+        </main>
+      );
+}
