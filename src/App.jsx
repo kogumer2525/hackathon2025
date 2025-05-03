6eb3ceeb1a79f6f9b80145d698b5ec1e1cf11f45
柳澤優貴愛 <yanagisawayuukiai@YukiAir.local>
2025-05-03 16:32:55 +0900

    バナーとリンクの互換性の追加、ファイル作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Schedule from "./pages/Schedule";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Events from "./pages/Events";
 import Members from "./pages/Members";
-import News from "./pages/News";
+import JoinUs from "./pages/JoinUs";
+import Contact from "./pages/Contact";
 import Links from "./pages/Links";
+
 
 export default function App() {
   return (
@@ -14,10 +18,15 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/schedule" element={<Schedule />} />
         <Route path="/members" element={<Members />} />
-        <Route path="/news" element={<News />} />
         <Route path="/links" element={<Links />} />
+        <Route path="/joinus" element={<JoinUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+
+       
       </Routes>
     </>
   );
