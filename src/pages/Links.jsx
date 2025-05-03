aadf15ba08c37956353f0e003a7627a3ba4a32bc
柳澤優貴愛 <yanagisawayuukiai@YukiAir.local>
2025-05-03 20:37:41 +0900

    Linksページの大枠

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -1,1 +1,12 @@
-export default function Links() {}
+export default function Links() {
+    return (
+        <main className="w-full min-h-screen bg-white">
+          <section className="py-16 px-6 bg-gradient-to-b from-green-100 to-white">
+            <h2 className="text-3xl md:text-5xl font-bold mb-4">Links</h2>
+            <p className="text-base md:text-lg text-gray-700 max-w-2xl">
+              ここにLinksページの内容が入るので記入お願いします
+            </p>
+          </section>
+        </main>
+      );
+}
