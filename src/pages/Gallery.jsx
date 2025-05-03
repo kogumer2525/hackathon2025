2c1556bd550413d2a87de1ef10c90ebcd636fa07
柳澤優貴愛 <yanagisawayuukiai@YukiAir.local>
2025-05-03 20:36:26 +0900

    Galleryページの大枠

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,1 +1,12 @@
-export default function Gallery() {}
+export default function Gallery() {
+    return (
+        <main className="w-full min-h-screen bg-white">
+          <section className="py-16 px-6 bg-gradient-to-b from-green-100 to-white">
+            <h2 className="text-3xl md:text-5xl font-bold mb-4">Gallery</h2>
+            <p className="text-base md:text-lg text-gray-700 max-w-2xl">
+              ここにGalleryページの内容が入るので記入お願いします
+            </p>
+          </section>
+        </main>
+      );
+}
